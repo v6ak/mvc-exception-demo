ba596491cf5b18c51fa1f86acae13de837a13066
Vít Šesták <vit6ak@gmail.com>
2010-08-13 21:51:33 +0200

    exception handling modiffied

--- presenters/BasePresenter.php
+++ presenters/BasePresenter.php
@@ -4,6 +4,8 @@ abstract class BasePresenter extends Presenter {
 	
 	public function templatePrepareFilters($template) {
 		$template->registerFilter('TemplateFilters::removePhp');
+		LatteMacros::$defaultMacros['part'] = '<?php try{ $%% = $presenter->getPart%%() ?>';
+		LatteMacros::$defaultMacros['/part'] = '<?php }catch(Exception $e){ Debug::processException($e, true);echo get_class($e);?><div class="error">Něco se nám tu pokakalo, počkejte, prosím, než to přebalíme.</div><?php }; ?>';
 		parent::templatePrepareFilters($template);
 	}
 	
